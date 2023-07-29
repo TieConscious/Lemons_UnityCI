e792728d991b02716cd9a0a3c9aed97509bc514c
M <muashims@gmail.com>
2023-07-29 12:34:57 -0700

    Added enemy observer

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,43 +1,79 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
-    public GameObject player;
-    public CanvasGroup exitbackgroundImageCanvasGroup;
+    [FormerlySerializedAs("exitbackgroundImageCanvasGroup")] public CanvasGroup victorybackgroundImageCanvasGroup;
+    public CanvasGroup defeatbackgroundImageCanvasGroup;
 
+    private PlayerCharacter _player;
     private bool _isPlayerAtExit;
     private float _timer;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<PlayerCharacter>();
+        Observer[] observers = FindObjectsOfType<Observer>();
+        foreach (Observer observer in observers)
+        {
+            observer.player = _player.head.transform;
+            observer.playerLayer = 1 << _player.gameObject.layer;
+            observer.OnPlayerCaught += DefeatEndLevel;
+        }
+    }
 
     private void Update()
     {
         if (_isPlayerAtExit)
         {
-            EndLevel();
+            VictoryEndLevel();
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player)
+        if (other.gameObject == _player.gameObject)
         {
             _isPlayerAtExit = true;
         }
     }
 
-    private void EndLevel()
+    private void DefeatEndLevel()
     {
-        _timer += Time.deltaTime;
-        exitbackgroundImageCanvasGroup.alpha = _timer / fadeDuration;
+        StartCoroutine(EndLevel(defeatbackgroundImageCanvasGroup, true));
+    }
 
-        if (_timer > fadeDuration + displayImageDuration)
+    private void VictoryEndLevel()
+    {
+        StartCoroutine(EndLevel(victorybackgroundImageCanvasGroup, false));
+    }
+
+    private IEnumerator EndLevel(CanvasGroup canvasGroup, bool shouldRestart)
+    {
+        while (_timer < fadeDuration)
         {
-            #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-            #else
+            _timer += Time.deltaTime;
+            canvasGroup.alpha = _timer / fadeDuration;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(displayImageDuration);
+
+        if (shouldRestart)
+        {
+             SceneManager.LoadScene(0);
+        }
+        else
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
                 Application.Quit();
-            #endif
+#endif
         }
     }
 }
