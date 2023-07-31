ac07e5a57b37014f77eee1f79b77d424fcc98585
M <muashims@gmail.com>
2023-07-30 20:01:56 -0700

    Upgraded to URP

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
+    public Transform[] waypoints;
+
+    private int _currentWaypointIndex;
+    private NavMeshAgent _navMeshAgent;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        _navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_navMeshAgent.remainingDistance < _navMeshAgent.stoppingDistance)
+        {
+            _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+            _navMeshAgent.SetDestination(waypoints[_currentWaypointIndex].position);
+        }
     }
 }
